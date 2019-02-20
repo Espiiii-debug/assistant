000f822322b614c4e1ac98e519158537ae450db3
Adri11334 <45499745+Adri11334@users.noreply.github.com>
2019-02-20 17:39:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -311,9 +311,9 @@ bot.on('message', function (message){
 			message.channel.bulkDelete(1)
 
 			const embed = new RichEmbed()
-				.setDescription("https://image.noelshack.com/fichiers/2019/08/3/1550679786-b6e47389-62f1-484b-a887-17581ad477a1.jpeg")
+				.setImage("https://image.noelshack.com/fichiers/2019/08/3/1550679786-b6e47389-62f1-484b-a887-17581ad477a1.jpeg")	
 				.setColor('#fd79a8')
-				.setFooter(`RIP Vador :cry: | 10/01/11 - 15/02/19 10h24`)
+				.setFooter(`RIP Vador | 10/01/11 - 15/02/19 10h24`)
 			message.channel.send(embed);
 		}
   	}
