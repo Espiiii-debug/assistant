922d4bfa25c079499ce30a1f32ad23f94eb40795
Adri11334 <45499745+Adri11334@users.noreply.github.com>
2019-02-20 17:35:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ bot.on('message', function (message){
 			message.channel.bulkDelete(1)
 
 			const embed = new RichEmbed()
-				.addField("", "https://image.noelshack.com/fichiers/2019/08/3/1550679786-b6e47389-62f1-484b-a887-17581ad477a1.jpeg")
+				.setDescription("https://image.noelshack.com/fichiers/2019/08/3/1550679786-b6e47389-62f1-484b-a887-17581ad477a1.jpeg")
 				.setColor('#fd79a8')
 				.setFooter(`RIP Vador :cry: | 10/01/11 - 15/02/19 10h24`)
 			message.channel.send(embed);
